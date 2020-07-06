610ace3e1379bad1f818c414b653abd050b3f105
miguel <email@address.com>
2020-07-06 11:50:09 +0200

    defect_splitter

---
+++ defect_splitter.py
@@ -0,0 +1,25 @@
+from dataset_generator import *
+from tqdm import tqdm
+import shutil, os
+
+DESTINY_PATH = 'Dataset/data_ree/bent_tower_images/'
+COMPLEMENTARY_PATH = 'Dataset/data_ree/complementary_images/'
+dataset = DefectDataset(BASE_PATH)
+
+try: 
+    os.mkdir(DESTINY_PATH)     
+except OSError as error: 
+    print(error)     
+
+try: 
+    os.mkdir(COMPLEMENTARY_PATH)     
+except OSError as error: 
+    print(error)     
+
+for image, mask , info in tqdm(dataset):
+    filename = info['image_filename'].split('/')[-1]
+    if 6 in info['defect_numbers']:
+        shutil.copy(info['image_filename'],DESTINY_PATH+filename)
+    else:
+        shutil.copy(info['image_filename'],COMPLEMENTARY_PATH+filename)
+
