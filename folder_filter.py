29392d5bf5d8df7dc8ac91c3dce5830f02d9018d
Miguel Fernandez-Cortizas <43600658+miferco97@users.noreply.github.com>
2020-07-23 13:32:25 +0200

    Update folder_filter.py
    tqdm added

--- folder_filter.py
+++ folder_filter.py
@@ -11,7 +11,7 @@ DESTINY_FOLDER_2 = './Dataset/destiny2/'
 def main():
     files_1 = os.listdir(ORIGIN_PATH_1)
     files_2 = os.listdir(ORIGIN_PATH_2)
-    for f in files_1:
+    for f in tqdm(files_1):
         if f in files_2:
             shutil.copy2(ORIGIN_PATH_1+f,DESTINY_FOLDER_1+f)
         else:
