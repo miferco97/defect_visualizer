1089751f20d1d551c531eb2eaf174d88f1994504
Miguel <miferco97@gmail.com>
2020-07-23 13:29:04 +0200

    folder_filter

---
+++ folder_filter.py
@@ -0,0 +1,21 @@
+import os,shutil
+from  tqdm import tqdm
+
+ORIGIN_PATH_1 = './Dataset/test1/'
+ORIGIN_PATH_2 = './Dataset/test2/'
+ 
+DESTINY_FOLDER_1 = './Dataset/destiny1/'
+DESTINY_FOLDER_2 = './Dataset/destiny2/'
+
+
+def main():
+    files_1 = os.listdir(ORIGIN_PATH_1)
+    files_2 = os.listdir(ORIGIN_PATH_2)
+    for f in files_1:
+        if f in files_2:
+            shutil.copy2(ORIGIN_PATH_1+f,DESTINY_FOLDER_1+f)
+        else:
+            shutil.copy2(ORIGIN_PATH_1+f,DESTINY_FOLDER_2+f)
+
+if __name__ == "__main__":
+    main()
